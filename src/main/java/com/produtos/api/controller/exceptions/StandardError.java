471441190a064ec1027d3395fd872cc982154bdd
Guilherme de Melo Cardoso <guilherme.cardoso@sankhya.com.br>
2021-05-06 02:29:54 -0300

    enviando classe StandardError que por algum motivo nao subiu

---
+++ src/main/java/com/produtos/api/controller/exceptions/StandardError.java
@@ -0,0 +1,32 @@
+package com.produtos.api.controller.exceptions;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer status_code;
+	private String message;
+	
+	public StandardError() {
+		
+	}
+
+	public Integer getStatus_code() {
+		return status_code;
+	}
+
+	public void setStatus_code(Integer status_code) {
+		this.status_code = status_code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
